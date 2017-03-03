f0629a6edd7a283fe9b7bd8831893ed3c541a1f0
Tiago Jesus <tiagofilipe12@gmail.com>
2017-03-03 10:10:04 +0000

    implemented a input search for length filter alongside with slider

--- modules/visualization_functions.js
+++ modules/visualization_functions.js
@@ -305,6 +305,83 @@ function onLoad() {
           });
           renderer.rerender();
         });
+
+        // inputs mins and maxs for slider
+        var inputMin = document.getElementById("slider_input_min"),
+            inputMax = document.getElementById("slider_input_max"),
+            inputs = [inputMin, inputMax]
+        slider.noUiSlider.on('update', function(values,handle ) {
+          inputs[handle].value = values[handle];
+        });
+
+        function setSliderHandle(i, value) {
+          var r = [null,null];
+          r[i] = value;
+          slider.noUiSlider.set(r);
+        }
+
+        // Listen to keydown events on the input field.
+        inputs.forEach(function(input, handle) {
+
+          input.addEventListener('change', function(){
+            setSliderHandle(handle, this.value);
+          });
+
+          input.addEventListener('keydown', function( e ) {
+
+            var values = slider.noUiSlider.get();
+            var value = Number(values[handle]);
+
+            // [[handle0_down, handle0_up], [handle1_down, handle1_up]]
+            var steps = slider.noUiSlider.steps();
+
+            // [down, up]
+            var step = steps[handle];
+
+            var position;
+
+            // 13 is enter,
+            // 38 is key up,
+            // 40 is key down.
+            switch ( e.which ) {
+
+              case 13:
+                setSliderHandle(handle, this.value);
+                break;
+
+              case 38:
+
+                // Get step to go increase slider value (up)
+                position = step[1];
+
+                // false = no step is set
+                if ( position === false ) {
+                  position = 1;
+                }
+
+                // null = edge of slider
+                if ( position !== null ) {
+                  setSliderHandle(handle, value + position);
+                }
+
+                break;
+
+              case 40:
+
+                position = step[0];
+
+                if ( position === false ) {
+                  position = 1;
+                }
+
+                if ( position !== null ) {
+                  setSliderHandle(handle, value - position);
+                }
+                break;
+            }
+          });
+        });
+
         // resets the slider
         $('#reset-sliders').click(function(event){
           slider.noUiSlider.set([min, max]);
