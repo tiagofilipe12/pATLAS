9fa832f0ca5f39e3ead108ede68a9f37b96d046e
Tiago Jesus <tiagofilipe12@gmail.com>
2017-03-13 16:08:31 +0000

    removal of links and nodes without any links implemented

--- modules/node_handling/node_removal_taxa.js
+++ modules/node_handling/node_removal_taxa.js
@@ -1,17 +1,29 @@
 // function to remove all nodes that have no default color
 function node_removal_taxa(g, graphics, nodeColor){
+  console.log("executing re run...")
+  var unchanged_nodes = [];
   g.forEachNode(function (node) {
-    //console.log(node.id)
-  	var nodeUI = graphics.getNodeUI(node.id);
-    console.log(nodeUI.color)
-    if (nodeUI.color == nodeColor){   // apparently this is the default assumed at this stage.
-      console.log(node.id)
-      g.removeNode(node.id);
+    console.log(node.links.length)
+    //console.log(node.links)
+    var nodeUI = graphics.getNodeUI(node.id);
+    // first check if nodeUI has default color
+    if (nodeUI.color == nodeColor){   // checks if color is equal do default
+      unchanged_nodes.push(node.id)
       g.forEachLinkedNode(node.id, function(linkedNode, link){
-        g.removeLink(link); 
-      });    
+        var linked_nodeUI = graphics.getNodeUI(linkedNode.id);
+        //console.log(String(link.id))
+        if (linked_nodeUI.color == nodeColor && nodeUI.color == nodeColor){ //if equal to default and linked nodes are equal to default
+          console.log("removing links")          
+          g.removeLink(link); 
+        }
+      });   
     }
+    // checks if node.links are empty
+    
+    if (jQuery.isEmptyObject(node.links[0])){
+      console.log("removing all nodes without any links")
+      g.removeNode(node.id)
+    };
   });
-  // funções para precompute durante um bocado e fazer desaparecer o gráfico momentaneamente com aquele loading again
 }
 
