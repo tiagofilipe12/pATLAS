bab19c753529382378ec94fac0ae471391e9f2f6
Tiago Jesus <tiagofilipe12@gmail.com>
2017-03-21 10:36:56 +0000

    added an option to execute buttons using keyboard with mousetrap.js

---
+++ modules/keyboard_control/keyboard_shortcuts.js
@@ -0,0 +1,21 @@
+// keyboard shortcuts using mousetrap.min.js from https://craig.is/killing/mice//
+
+// keyboard shortcut for length filter
+Mousetrap.bind("shift+l", function(e){
+  document.getElementById('length_filter').click();
+});
+
+// keyboard shortcut for taxa filter
+Mousetrap.bind("shift+t", function(e){
+  document.getElementById('taxa_filter').click();
+});
+
+// keyboard shortcut for play/pause rendering
+Mousetrap.bind("shift+p", function(e){
+  $('#playpauseButton').click();
+});
+
+// keyboard shortcut for reset all filters
+Mousetrap.bind("shift+r", function(e){
+  $('#reset-sliders').click();
+});
