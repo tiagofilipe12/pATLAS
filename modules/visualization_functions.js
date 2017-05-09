6282fd978d688f0e61e2802285ffc91fe543df1d
Tiago Jesus <tiagofilipe12@gmail.com>
2017-05-09 16:27:09 +0100

    remove accordion functions

--- modules/visualization_functions.js
+++ modules/visualization_functions.js
@@ -936,17 +936,4 @@ function onLoad () {
     assembly_json = new_assembly_json
   })
 
-    // Collapse accordion every time dropdown is shown
-  $('.dropdown-accordion').on('show.bs.dropdown', function () {
-    var accordion = $(this).find($(this).data('accordion'));
-    accordion.find('.panel-collapse.in').collapse('hide');
-  });
-
-  // Prevent dropdown to be closed when we click on an accordion link
-  $('.dropdown-accordion').on('click', 'a[data-toggle="collapse"]', function (event) {
-    event.preventDefault();
-    event.stopPropagation();
-    $($(this).data('parent')).find('.panel-collapse.in').collapse('hide');
-    $($(this).attr('href')).collapse('show');
-  })
 }
