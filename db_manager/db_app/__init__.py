d00d9b62be4305d9eb9903804bac2f08f4901d04
Tiago Jesus <tiagofilipe12@gmail.com>
2017-05-15 17:14:53 +0100

    add models and marshmallow integration

--- db_manager/db_app/__init__.py
+++ db_manager/db_app/__init__.py
@@ -1,9 +1,27 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 import os
 
 app = Flask(__name__)
 app.config.from_pyfile(os.path.join('..', 'config_default.py'))
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
 import views, models
+
+class PlasmidSchema(ma.ModelSchema):
+    class Meta:
+        model = Plasmid
+
+class CardSchema(ma.ModelSchema):
+    class Meta:
+        model = Card
+
+class PositiveSchema(ma.ModelSchema):
+    class Meta:
+        model = Positive
+
+class DatabaseSchema(ma.ModelSchema):
+    class Meta:
+        model = Database
