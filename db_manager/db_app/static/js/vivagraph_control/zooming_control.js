36f2e014225fe84a43759d2b125ea6225e094588
tiagofilipe12 <tiagofilipe12@gmail.com>
2017-09-06 15:49:01 +0100

    added default zooming to get all the graph

---
+++ db_manager/db_app/static/js/vivagraph_control/zooming_control.js
@@ -0,0 +1,22 @@
+// Final bit: most likely graph will take more space than available
+// screen. Let's zoom out to fit it into the view:
+
+const defaultZooming = (layout,renderer) => {
+  var graphRect = layout.getGraphRect()
+  var graphSize = Math.min(graphRect.x2 - graphRect.x1, graphRect.y2 - graphRect.y1)
+  var screenSize = Math.min(document.body.clientWidth, document.body.clientHeight)
+  var desiredScale = screenSize / graphSize
+
+  const zoomOut = (desiredScale, currentScale) => {
+    // zoom API in vivagraph 0.5.x is silly. There is no way to pass transform
+    // directly. Maybe it will be fixed in future, for now this is the best I could do:
+    if (desiredScale < currentScale) {
+      currentScale = renderer.zoomOut();
+      setTimeout(function () {
+        zoomOut(desiredScale, currentScale);
+      }, 16);
+    }
+  }
+
+  zoomOut(desiredScale, 1)
+}
