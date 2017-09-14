f936a20ea0befe1168ded11569fdfd7514341731
tiagofilipe12 <tiagofilipe12@gmail.com>
2017-09-14 17:40:50 +0100

    implemented node rendering without duplicated links with no need for hashes

--- db_manager/db_app/static/js/visualization_functions.js
+++ db_manager/db_app/static/js/visualization_functions.js
@@ -95,15 +95,12 @@ const onLoad = () => {
 
               // loops between all arrays of array pairing sequence and distances
               for (let i = 0; i < dict_dist.length; i++) {
-                const pairs = dict_dist[i]
-                const reference = pairs[0].split('_').slice(0, 3).join('_')  // stores references in a unique variable
-                const distance = pairs[1]   // stores distances in a unique variable
-                // assures that link wasn't previously added
-                const currentHash = makeHash(sequence, reference)
-                if (listHashes.indexOf(currentHash) < 0) {
-                  g.addLink(sequence, reference, distance)
-                  listHashes.push(currentHash)
-                }
+                //console.log(dict_dist[i], Object.keys(dict_dist[i])[0])
+                //const pairs = dict_dist[i]
+                const reference = Object.keys(dict_dist[i])[0]  // stores references in a unique variable
+                //console.log(Object.values(dict_dist[i])[0].distance)
+                const distance = Object.values(dict_dist[i])[0].distance   // stores distances in a unique variable
+                g.addLink(sequence, reference, distance)
               }
             }
             // checks if the node is the one with most links and stores it in
