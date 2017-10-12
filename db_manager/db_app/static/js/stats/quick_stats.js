fc1de36b322edc0072eb2783b16dc5cafe3f5d3f
tiagofilipe12 <tiagofilipe12@gmail.com>
2017-10-12 11:47:58 +0100

    fixed undefined variables

--- db_manager/db_app/static/js/stats/quick_stats.js
+++ db_manager/db_app/static/js/stats/quick_stats.js
@@ -40,44 +40,43 @@ const getMetadata = (tempList) => {
   const speciesList = []
   const speciesObject = {}
   for (const item in tempList) {
-    nodeId = tempList[item]
-    $.get("api/getspecies/", {"accession": nodeId}, (data, status) => {
-      // this request uses nested json object to access json entries
-      // available in the database
-      // if request return no speciesName or plasmidName
-      // sometimes plasmids have no descriptor for one of these or both
-      if (data.json_entry.name === null) {
-        speciesName = "unknown"
-      } else {
-        speciesName = data.json_entry.name.split("_").join(" ")
-      }
-      // push to main list to control the final of the loop
-      speciesList.push(speciesName)
-      // constructs the speciesObject object that counts the number of
-      // occurrences of a species
-      if (!(speciesName in speciesObject)) {
-        speciesObject[speciesName] = 1
-      } else {
-        speciesObject[speciesName] = speciesObject[speciesName] + 1
-      }
-      // if speciesList reaches the size of accessions given to tempList
-      // EXECUTE STATS
-      const layout = {
-        yaxis: {
-          title: "Number of selected plasmids"
-        },
-        xaxis: {
-          title: "Species",
-          tickangle: -45
-        },
-        title: "Species in selection",
-        margin: {
-          b: 200,
-          l: 100
+    if ({}.hasOwnProperty.call(tempList, item)) {
+      const nodeId = tempList[item]
+      $.get("api/getspecies/", {"accession": nodeId}, (data, status) => {
+        // this request uses nested json object to access json entries
+        // available in the database
+        // if request return no speciesName or plasmidName
+        // sometimes plasmids have no descriptor for one of these or both
+        const speciesName = (data.json_entry.name === null) ?
+          "unknown" : data.json_entry.name.split("_").join(" ")
+        // push to main list to control the final of the loop
+        speciesList.push(speciesName)
+        // constructs the speciesObject object that counts the number of
+        // occurrences of a species
+        if (!(speciesName in speciesObject)) {
+          speciesObject[speciesName] = 1
+        } else {
+          speciesObject[speciesName] = speciesObject[speciesName] + 1
         }
-      }
-      if (speciesList.length === tempList.length) { statsParser(speciesList, layout, true) }
-    })
+        // if speciesList reaches the size of accessions given to tempList
+        // EXECUTE STATS
+        const layout = {
+          yaxis: {
+            title: "Number of selected plasmids"
+          },
+          xaxis: {
+            title: "Species",
+            tickangle: -45
+          },
+          title: "Species in selection",
+          margin: {
+            b: 200,
+            l: 100
+          }
+        }
+        if (speciesList.length === tempList.length) { statsParser(speciesList, layout, true) }
+      })
+    }
   }
 }
 
@@ -85,45 +84,44 @@ const getMetadataLength = (tempList) => {
   const lengthList = []
   const lengthObject = {}
   for (const item in tempList) {
-    nodeId = tempList[item]
-    $.get("api/getspecies/", {"accession": nodeId}, (data, status) => {
-      // this request uses nested json object to access json entries
-      // available in the database
+    if ({}.hasOwnProperty.call(tempList, item)) {
+      const nodeId = tempList[item]
+      $.get("api/getspecies/", {"accession": nodeId}, (data, status) => {
+        // this request uses nested json object to access json entries
+        // available in the database
 
-      // get data for length
-      if (data.json_entry.length === null) {
-        speciesLength = "unknown"
-      } else {
-        speciesLength = data.json_entry.length
-      }
-      // push to main list to control the final of the loop
-      lengthList.push(speciesLength)
-      // constructs the lengthObject that counts the number of occurrences
-      // of a given distribution
-      // TODO this should be categorical --> Some user provided param
-      if (!(speciesLength in lengthObject)) {
-        lengthObject[speciesLength] = 1
-      } else {
-        lengthObject[speciesLength] = lengthObject[speciesLength] + 1
-      }
-      // if speciesList reaches the size of accessions given to tempList
-      // EXECUTE STATS
-      const layout = {
-        yaxis: {
-          title: "Number of selected plasmids"
-        },
-        xaxis: {
-          title: "Length",
-          tickangle: -45
-        },
-        title: "Lengths in selection",
-        margin: {
-          b: 200,
-          l: 100
+        // get data for length
+        const speciesLength = (data.json_entry.length === null) ?
+          "unknown" : data.json_entry.length
+        // push to main list to control the final of the loop
+        lengthList.push(speciesLength)
+        // constructs the lengthObject that counts the number of occurrences
+        // of a given distribution
+        // TODO this should be categorical --> Some user provided param
+        if (!(speciesLength in lengthObject)) {
+          lengthObject[speciesLength] = 1
+        } else {
+          lengthObject[speciesLength] = lengthObject[speciesLength] + 1
         }
-      }
-      if (lengthList.length === tempList.length) { statsParser(lengthList, layout, false) }
-    })
+        // if speciesList reaches the size of accessions given to tempList
+        // EXECUTE STATS
+        const layout = {
+          yaxis: {
+            title: "Number of selected plasmids"
+          },
+          xaxis: {
+            title: "Length",
+            tickangle: -45
+          },
+          title: "Lengths in selection",
+          margin: {
+            b: 200,
+            l: 100
+          }
+        }
+        if (lengthList.length === tempList.length) { statsParser(lengthList, layout, false) }
+      })
+    }
   }
 }
 
