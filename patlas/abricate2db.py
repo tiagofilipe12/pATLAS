17d224b1a2fdc9debe771bed062728ce5bd108c1
tiagofilipe12 <tiagofilipe12@gmail.com>
2017-10-27 10:00:08 +0100

    kept filtering the dicts generated by process_abricate.py

--- patlas/abricate2db.py
+++ patlas/abricate2db.py
@@ -9,34 +9,57 @@ except ImportError:
     from patlas.db_manager.db_app import db, models
     from patlas.templates.process_abricate import Abricate
 
-# class DbInsertion(ParentClass):
-#     '''
-#     class with DB specific methods
-#     This class expects to inherit a storage dictionary from its parent class
-#     in which the keys are accession numbers and the values are a list of all
-#     the hits from abricate for a given accession.
-#     '''
-#
-#     def __init__(self, var):
-#         # Here we'll call the __init__ of the base class Parent.
-#         super().__init__(var)
-#         print("Base class parent ")
-#
-#     def get_storage(self):
-#         '''
-#         Notice that the self.storage attribute is available, even
-#         though it was not defined in the DB_INSERTATRON class.
-#         '''
-#
-#         for key, vals in self.storage.items():
-#             row = models.Card(
-#                 plasmid_id = key,
-#                 json_entry = vals
-#             )
-#             db.session.add(row)
-#             db.session.commit()
-#         # close db in the end of get_storage
-#         db.session.close()
+class DbInsertion(Abricate):
+    '''
+    class with DB specific methods
+    This class expects to inherit a storage list of dictionaries from its
+    Abricate parent class
+    '''
+
+    def __init__(self, var):
+        # Here we'll call the __init__ of the base class Abricate.
+        super().__init__(var)
+
+    def get_storage(self, input_file, list_of_filters):
+        print(self)
+
+        abr = Abricate(self)
+
+        # self.get_items(input_file, list_of_filters)
+        for entry in abr.iter_filter(list_of_filters, fields=[
+            "reference",
+            "coverage",
+            "identity",
+            "database",
+            "gene",
+            "accession",
+            "seq_range"]):
+            print(entry)
+            print(entry["reference"])
+            reference_accession = "_".join(entry["reference"].split("_")[0:3])
+            del entry["reference"]
+            print(entry)
+            print(reference_accession)
+            break
+
+        '''
+        Notice that the self.storage attribute is available, even
+        though it was not defined in the DB_INSERTATRON class.
+        '''
+
+    # def parse_storage(self, dict):
+    #     print(self)
+    #
+    #     for key, vals in self.dict.items():
+    #         print(key,vals)
+        #     row = models.Card(
+        #         plasmid_id = key,
+        #         json_entry = vals
+        #     )
+        #     db.session.add(row)
+        #     db.session.commit()
+        # # close db in the end of get_storage
+        # db.session.close()
 
 # TODO convert main into a moduole that can be imported by MASHix.py?
 def main():
@@ -72,22 +95,15 @@ def main():
 
     print(input_file)
 
-    abr = Abricate(input_file)
-
     list_of_filters = [
         ["coverage", ">=", perc_cov],
         ["identity", ">=", perc_id]
     ]
 
-    for i in abr.iter_filter(list_of_filters, fields=["reference",
-                                                      "coverage",
-                                                      "identity",
-                                                      "database",
-                                                      "gene",
-                                                      "accession",
-                                                      "seq_range"]):
-        print(i)
-        break
+    DbInsertion.get_storage(input_file, input_file, list_of_filters)
+
+
+
 
     # Function to read the input and save a sequence, and a list of all [
     # resistances found with their id and cov (json like) as shown below.
