7b707c91631c606db76ecc9d74b6079afb98f4d3
tiagofilipe12 <tiagofilipe12@gmail.com>
2017-11-08 08:17:50 +0000

    added new views for resistance and plasmidfinder

--- patlas/db_manager/db_app/views.py
+++ patlas/db_manager/db_app/views.py
@@ -6,38 +6,58 @@ from flask import json, render_template
 
 ## routes
 
-@app.route('/')
-@app.route('/index')
+@app.route("/")
+@app.route("/index")
 def index():
     return render_template("index.html")
 
-@app.route('/test')
+@app.route("/test")
 def main_summary():
-    data = make_summary('db_app/static/json/import_to_vivagraph_v5.json')
+    data = make_summary("db_app/static/json/import_to_vivagraph_v5.json")
     response = app.response_class(
         response=json.dumps(data),
         status=200,
-        mimetype='application/json'
+        mimetype="application/json"
     )
     return response
 
-@app.route('/fullDS')
+@app.route("/fullDS")
 def full_ds():
-    data = make_summary('db_app/static/json/filtered.json')
+    data = make_summary("db_app/static/json/filtered.json")
     response = app.response_class(
         response=json.dumps(data),
         status=200,
-        mimetype='application/json'
+        mimetype="application/json"
     )
     return response
 
-@app.route('/taxa')
+@app.route("/taxa")
 def taxa_summary():
-    data = make_summary('db_app/static/json/taxa_tree.json')
+    data = make_summary("db_app/static/json/taxa_tree.json")
     response = app.response_class(
         response=json.dumps(data),
         status=200,
-        mimetype='application/json'
+        mimetype="application/json"
+    )
+    return response
+
+@app.route("/resistance")
+def res_summary():
+    data = make_summary("db_app/static/json/resistance.json")
+    response = app.response_class(
+        response=json.dumps(data),
+        status=200,
+        mimetype="application/json"
+    )
+    return response
+
+@app.route("/plasmidfinder")
+def pf_summary():
+    data = make_summary("db_app/static/json/plasmidfinder.json")
+    response = app.response_class(
+        response=json.dumps(data),
+        status=200,
+        mimetype="application/json"
     )
     return response
 
