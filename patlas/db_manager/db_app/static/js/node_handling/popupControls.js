a6dfabffb779b2b3345d9648925914836849ed96
tiagofilipe12 <tiagofilipe12@gmail.com>
2017-11-21 16:06:12 +0000

    added highlight for queried node and support to open popup_description

--- patlas/db_manager/db_app/static/js/node_handling/popupControls.js
+++ patlas/db_manager/db_app/static/js/node_handling/popupControls.js
@@ -13,3 +13,78 @@ const arrayToCsv = (array) => {
   document.body.appendChild(link) // Required for FF
   link.click() // This will download the data file named "my_data.csv".
 }
+
+const setupPopupDisplay = (node, speciesName, plasmidName) => {
+  // first needs to empty the popup in order to avoid having
+  // multiple entries from previous interactions
+  $("#popup_description").empty()
+  $("#popup_description").append(
+    "<button id='close' class='btn btn-default' type='button'>&times;</button>" +
+    "<button class='btn btn-default' id='downloadCsv'" +
+    "type='button' data-toogle='tooptip'" +
+    "title='Export as csv'>" +
+    "<span class='glyphicon glyphicon-save-file'></span>" +
+    "</button>" +
+    "<div>General sequence info" +
+    "<div id='accessionPop'>" +
+    node.data.sequence + "</div>" +
+    "<div id='speciesNamePop'><span style='color: #468499'>Species:" +
+    " </span>" + speciesName +
+    "</div>" + node.data.seq_length +
+    "<div id='plasmidNamePop'>" +
+    "<span style='color: #468499'>Plasmid: </span>" + plasmidName +
+    "</div><div id='percentagePop'>" +
+    "<span style='color: #468499'>Percentage:" +
+    " </span>" + node.data.percentage +
+    "</div><div id='copyNumberPop'>" +
+    "<span style='color: #468499'>Relative copy number: " +
+    "</span>" + node.data.copyNumber +
+    "</div>" +
+    // adds buttons for resistances and plasmid families
+    "<br />" +
+    "<div style='float: left;' class='btn btn-default'" +
+    " id='resButton'>" +
+    " Resistances" +
+    "</div>" +
+    "<div style='float: right;' class='btn btn-default'" +
+    " id='plasmidButton'>" +
+    "Plasmid families" +
+    "</div>" +
+    "</div>"
+  )
+  $("#popup_description").show()
+}
+
+// call the requests
+const requestPlasmidTable = (node, setupPopupDisplay) => {
+  // if statement to check if node is in database or is a new import
+  // from mapping
+  if (node.data.seq_length) {
+    $.get('api/getspecies/', {'accession': node.id}, (data, status) => {
+      // this request uses nested json object to access json entries
+      // available in the database
+      // if request return no speciesName or plasmidName
+      // sometimes plasmids have no descriptor for one of these or both
+      if (data.json_entry.name === null) {
+        speciesName = "N/A"
+      } else {
+        speciesName = data.json_entry.name.split("_").join(" ")
+      }
+      if (data.json_entry.plasmid_name === null) {
+        plasmidName = "N/A"
+      } else {
+        plasmidName = data.json_entry.plasmid_name
+      }
+      // check if data can be called as json object properly from db something like data.species or data.length
+      setupPopupDisplay(node, speciesName, plasmidName) //callback
+      // function for
+      // node displaying after fetching data from db
+    })
+  }
+  // exception when node has no length (used on new nodes?)
+  else {
+    speciesName = 'N/A'
+    plasmidName = 'N/A'
+    setupPopupDisplay(node, speciesName, plasmidName) //callback
+  }
+}
--- patlas/db_manager/db_app/static/js/node_handling/toggle_nodes.js
+++ patlas/db_manager/db_app/static/js/node_handling/toggle_nodes.js
@@ -1,5 +1,4 @@
 const toggle_manager = (toggle_status) => {
-  // TODO change this to change the behavior of this button
   // if node mode on disable dropdown and retrieve an alert whenever the dropdown is clicked in this instance
   if (toggle_status === true) {
     document.getElementById("toggle-event").className += " disabled"
@@ -16,7 +15,7 @@ const toggle_manager = (toggle_status) => {
 }
 
 // function that iterates all nodes and when it finds a match recenters the dom
-const centerToggleQuery = (g, graphics, renderer, query) => {
+const centerToggleQuery = (g, graphics, renderer, query, currentQueryNode) => {
   g.forEachNode( (node) => {
     const nodeUI = graphics.getNodeUI(node.id)
     const sequence = node.data.sequence.split(">")[3].split("<")[0]
@@ -27,16 +26,35 @@ const centerToggleQuery = (g, graphics, renderer, query) => {
     if (sequence === query) {
       // centers graph visualization in a given node, searching for gi
       renderer.moveTo(x, y)
+      nodeUI.backupColor = nodeUI.color
+      nodeUI.color = 0x900C3F
+      // this sets the popup internal buttons to allow them to run,
+      // otherwise they won't run because its own function returns this
+      // variable to false, preveting the popup to expand with its
+      // respectiv functions
+      clickedPopupButtonCard = true
+      clickedPopupButtonRes = true
+      clickedPopupButtonFamily = true
+      // requests table for sequences metadata
+      requestPlasmidTable(node, setupPopupDisplay)
+      // also needs to reset previous node to its original color
+      if (window.currentQueryNode) {
+        previousNodeUI = graphics.getNodeUI(currentQueryNode)
+        previousNodeUI.color = previousNodeUI.backupColor
+      }
+      renderer.rerender()
     }
   })
+  return query
 }
 
 // function to search plasmidnames when toggle is on
-const toggleOnSearch = (g, graphics, renderer) => {
+const toggleOnSearch = (g, graphics, renderer, currentQueryNode) => {
   const query = $("#formValueId").val()
   $.get("api/getplasmidname/", {"plasmid_name": query})
     .then( (results) => {
       const centerAccession = results.plasmid_id
-      centerToggleQuery(g, graphics, renderer, centerAccession)
+      centerToggleQuery(g, graphics, renderer, centerAccession, currentQueryNode)
     })
+  return query
 }
