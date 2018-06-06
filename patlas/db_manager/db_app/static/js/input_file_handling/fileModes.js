acb103a82a46dbf1e0db488ce69a020faf6e625c
tiagofilipe12 <tiagofilipe12@gmail.com>
2018-06-06 16:34:10 +0100

    cleaned function mappingHighlights

--- patlas/db_manager/db_app/static/js/input_file_handling/fileModes.js
+++ patlas/db_manager/db_app/static/js/input_file_handling/fileModes.js
@@ -4,118 +4,101 @@ previousTableList, readFilejson, nodeColor, listGi, fileChecks,
 masterReadArray, selector, areaSelection, showDiv, mashJson, assemblyJson,
 listGiFilter, typeOfProject*/
 
+
+/**
+ * Function that parses the nodes to be highlighted and handles the globals
+ * required for posterior analysis or filters
+ * @param {Object} g
+ * @param {Object} graphics
+ * @param {Object} renderer
+ */
 const mappingHighlight = (g, graphics, renderer) => {
 
   // feeds the first file
-  const readString = (typeof Object.values(readFilejson)[0] === "string") ?
-    JSON.parse(Object.values(readFilejson)[0]) : Object.values(readFilejson)[0]
-
-  masterReadArray = []
+  const readString = (Object.keys(readFilejson).length === 0) ?
+    false : (typeof Object.values(readFilejson)[0] === "string") ?
+      JSON.parse(Object.values(readFilejson)[0]) : Object.values(readFilejson)[0]
 
   fileChecks(readString)
 
-  if (mashJson !== false) {
-    // masterReadArray = []
-    assemblyJson = false
-    // readFilejson = mashJson // converts mashJson into readFilejson to
-    // const readString = JSON.parse(Object.values(mashJson)[0])
-    // fileChecks(readString)
-    $("#fileNameDiv").html(Object.keys(mashJson)[0])
-      .show()
+  masterReadArray = []
 
-    // it and use the same function (readColoring)
-    resetAllNodes(graphics, g, nodeColor, renderer)
-    previousTableList = []
-    // transform selector object that handles plots and hide their
-    // respective divs
-    Object.keys(selector).map((el) => {
-      selector[el].state = false
-    })
-    hideAllOtherPlots()
-    areaSelection = false
+  resetAllNodes(graphics, g, nodeColor, renderer)
 
-    showDiv().then( () => {
-      const outputList = readColoring(g, listGi, graphics, renderer, readString)
-      listGi = outputList[0]
-      listGiFilter = outputList[1]
+  previousTableList = []
 
-      // adds mash screen queries to the typeOfProject
-      typeOfProject["mashscreen"] = readFilejson
+  Object.keys(selector).map((el) => {
+    selector[el].state = false
+  })
 
-      masterReadArray = pushToMasterReadArray(readFilejson)
+  hideAllOtherPlots()
 
-      hideDivsFileInputs()
-    })
+  areaSelection = false
 
-  } else if (assemblyJson !== false) {
+  if (readString) {
+    if (mashJson !== false) {
 
-    // const readString = JSON.parse(Object.values(assemblyJson)[0])
-    // fileChecks(readString)
-    $("#fileNameDiv").html(Object.keys(assemblyJson)[0])
-      .show()
-    // masterReadArray = []
-    // readFilejson = assemblyJson
-    resetAllNodes(graphics, g, nodeColor, renderer)
-    previousTableList = []
-    // transform selector object that handles plots and hide their
-    // respective divs
-    Object.keys(selector).map( (el) => { selector[el].state = false })
-    hideAllOtherPlots()
-    areaSelection = false
-    $("#loading").show()
-    showDiv().then( () => {
-      const outputList = readColoring(g, listGi, graphics, renderer, readString)
-      listGi = outputList[0]
-      listGiFilter = outputList[1]
+      assemblyJson = false
 
-      // adds mash screen queries to the typeOfProject
-      typeOfProject["assembly"] = readFilejson
+      $("#fileNameDiv").html(Object.keys(mashJson)[0])
+        .show()
 
-      masterReadArray = pushToMasterReadArray(readFilejson)
+      showDiv().then(() => {
+        const outputList = readColoring(g, listGi, graphics, renderer, readString)
+        listGi = outputList[0]
+        listGiFilter = outputList[1]
 
-      hideDivsFileInputs()
+        // adds mash screen queries to the typeOfProject
+        typeOfProject["mashscreen"] = readFilejson
 
-    })
+        masterReadArray = pushToMasterReadArray(readFilejson)
 
-  } else if (readFilejson !== false) {
-    // masterReadArray = []
-    assemblyJson = false
-    // // feeds the first file
-    // const readString = (typeof Object.values(readFilejson)[0] === "string") ?
-    //   JSON.parse(Object.values(readFilejson)[0]) : Object.values(readFilejson)[0]
-    // const readString = Object.values(readFilejson)[0]
+        hideDivsFileInputs()
+      })
 
-    // fileChecks(readString)
-    
-    $("#fileNameDiv").html(Object.keys(readFilejson)[0])
-      .show()
+    } else if (assemblyJson !== false) {
 
-    resetAllNodes(graphics, g, nodeColor, renderer)
-    previousTableList = []
-    // transform selector object that handles plots and hide their
-    // respective divs
-    Object.keys(selector).map((el) => {
-      selector[el].state = false
-    })
-    hideAllOtherPlots()
-    areaSelection = false
-    // $("#loading").show()
-    showDiv().then( () => {
-      // colors each node for first element of readFilejson
-      const outLists = readColoring(g, listGi, graphics, renderer, readString)
-      listGi = outLists[0]
-      listGiFilter = outLists[1]
+      $("#fileNameDiv").html(Object.keys(assemblyJson)[0])
+        .show()
 
-      // adds read queries to the typeOfProject
-      typeOfProject["mapping"] = readFilejson
+      showDiv().then(() => {
+        const outputList = readColoring(g, listGi, graphics, renderer, readString)
+        listGi = outputList[0]
+        listGiFilter = outputList[1]
 
-      masterReadArray = pushToMasterReadArray(readFilejson)
+        // adds mash screen queries to the typeOfProject
+        typeOfProject["assembly"] = readFilejson
 
-      hideDivsFileInputs()
-    })
+        masterReadArray = pushToMasterReadArray(readFilejson)
 
-  } else {
-    // alert user that file may be empty or there is no imported file at all
-    fileChecks(readFilejson)
+        hideDivsFileInputs()
+
+      })
+
+    } else if (Object.keys(readFilejson).length > 0 || readFilejson !== false) {
+
+      assemblyJson = false
+
+      $("#fileNameDiv").html(Object.keys(readFilejson)[0])
+        .show()
+
+      showDiv().then(() => {
+        // colors each node for first element of readFilejson
+        const outLists = readColoring(g, listGi, graphics, renderer, readString)
+        listGi = outLists[0]
+        listGiFilter = outLists[1]
+
+        // adds read queries to the typeOfProject
+        typeOfProject["mapping"] = readFilejson
+
+        masterReadArray = pushToMasterReadArray(readFilejson)
+
+        hideDivsFileInputs()
+      })
+
+    } else {
+      // alert user that file may be empty or there is no imported file at all
+      fileChecks(readFilejson)
+    }
   }
 }
