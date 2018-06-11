302ce3c068818c3a0eee75f51beec892125f89ee
tiagofilipe12 <tiagofilipe12@gmail.com>
2018-06-11 19:05:04 +0100

    documented functions and added minor fixes

--- patlas/db_manager/config_default.py
+++ patlas/db_manager/config_default.py
@@ -2,5 +2,5 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 ## or 'postgresql://localhost/plasmid_db'
-SQLALCHEMY_DATABASE_URI = "postgresql:///plasmid_db"
+SQLALCHEMY_DATABASE_URI = "postgresql:///plasmid_db_dev"
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, "db_repository")
