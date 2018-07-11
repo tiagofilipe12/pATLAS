57ff3e10af4837541cb70d7e0edfa760d1e925f9
tiagofilipe12 <tiagofilipe12@gmail.com>
2018-07-11 13:59:23 +0100

    added option to set database name in db_create.py script

--- patlas/db_manager/config_default.py
+++ patlas/db_manager/config_default.py
@@ -1,9 +1,16 @@
 import os
+import sys
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+# try except statement to set database name if none is provided then use default
+# 'plasmid_db_stable'.
+try:
+    db_name = sys.argv[1]
+except IndexError:
+    db_name = "plasmid_db_stable"
+
+
 ## or 'postgresql://localhost/plasmid_db'
-SQLALCHEMY_DATABASE_URI = "postgresql:///plasmid_db_stable"
-# sql database for development, uncomment to use it.
-#SQLALCHEMY_DATABASE_URI = "postgresql:///plasmid_db_dev"
+SQLALCHEMY_DATABASE_URI = "postgresql:///{}".format(db_name)
 
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, "db_repository")
