6989fe28b3a9532f68b57f111a544df96303f272
Tiago Jesu <tiagofilipe12@gmail.com>
2018-09-04 17:58:54 +0100

    allow external access

--- patlas/db_manager/db_app/views.py
+++ patlas/db_manager/db_app/views.py
@@ -51,6 +51,16 @@ def repetitive_function(path):
     return response
 
 
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers',
+                       'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods',
+                       'GET,PUT,POST,DELETE,OPTIONS')
+    return response
+
+
 # routes
 @app.route("/")
 @app.route("/index")
